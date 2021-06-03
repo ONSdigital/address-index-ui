c870f8c3d2d008be06326108476d81a2466638ce
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-03 10:05:20 +0100

    renamed app to be __init__

--- src/aims_ui/__init__.py
+++ src/aims_ui/__init__.py
@@ -1,1 +1,21 @@
+from flask import Flask
+import os
+import aims_ui.config.base as config_base
 
+app = Flask(__name__)
+
+ENV = os.environ.get('FLASK_ENV')
+
+if ENV=='development':
+    import aims_ui.config.development as config_env
+elif ENV=='test':
+    import aims_ui.config.test as config_env
+elif ENV=='prod':
+    import aims_ui.config.prod as config_env
+else:
+    raise Exception('invalid environment ' + ENV)
+
+app.config.from_object(config_base)
+app.config.from_object(config_env)
+
+print(app.config['DATABASE_URI'])
