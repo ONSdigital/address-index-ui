7d149ad4ad5a4a38fd7537d2d689bffd0345e849
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-04 13:59:06 +0100

    Cleaned up environment variable loading

--- src/aims_ui/config/prod.py
+++ src/aims_ui/config/prod.py
@@ -1,7 +1,5 @@
 """FLASK PROD CONFIG"""
 import os
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
 if not (SECRET_KEY := os.getenv('SECRET_KEY')):
     raise RuntimeError('no SECRET_KEY ENV variable set')
--- src/aims_ui/config/test.py
+++ src/aims_ui/config/test.py
@@ -1,7 +1,5 @@
 """FLASK TEST CONFIG"""
 import os
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
 if not (SECRET_KEY := os.getenv('SECRET_KEY')):
     raise RuntimeError('no SECRET_KEY ENV variable set')
