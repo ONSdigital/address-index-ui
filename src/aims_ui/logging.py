e0f9ed1e9020233cf7c65417cbdc363fc6168265
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-06 16:03:50 +0100

    Ran reformatter

--- src/aims_ui/logging.py
+++ src/aims_ui/logging.py
@@ -1,17 +1,23 @@
 from logging.config import dictConfig
 
+
 def setup_logging(platform):
 
   dictConfig({
       'version': 1,
-      'formatters': {'default': {
-          'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-      }},
-      'handlers': {'wsgi': {
-          'class': 'logging.StreamHandler',
-          'stream': 'ext://sys.stdout', 
-          'formatter': 'default'
-      }},
+      'formatters': {
+          'default': {
+              'format':
+              '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+          }
+      },
+      'handlers': {
+          'wsgi': {
+              'class': 'logging.StreamHandler',
+              'stream': 'ext://sys.stdout',
+              'formatter': 'default'
+          }
+      },
       'root': {
           'level': 'INFO',
           'handlers': ['wsgi']
@@ -24,5 +30,4 @@ def setup_logging(platform):
     client = google.cloud.logging.Client()
 
     client.get_default_handler()
-    client.setup_logging()  
-
+    client.setup_logging()
