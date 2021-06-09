acf7c255b8d91f5b8c0c7840b923a3f54837ac82
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-09 10:48:23 +0100

    Added testinig groundwork

---
+++ tests/test_flaskr.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+
+import pytest
+
+from src import aims_ui
+
+
+@pytest.fixture
+def client():
+    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+    flaskr.app.config['TESTING'] = True
+
+    with flaskr.app.test_client() as client:
+        with flaskr.app.app_context():
+            flaskr.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(flaskr.app.config['DATABASE'])
