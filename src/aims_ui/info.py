c87747130ee807e81d38b46fb66add8a0d34c4fa
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-09 14:02:23 +0100

    addedversiondetectin

--- src/aims_ui/info.py
+++ src/aims_ui/info.py
@@ -1,12 +1,18 @@
 import os
 from . import app
 from flask import jsonify
+from importlib_metadata import version
 
+def get_version():
+  print(version('aims_ui'))
+
+  return "s"
 
 @app.route('/info')
 def info():
   key_info = {}
   key_info['ENV'] = os.getenv('FLASK_ENV')
   key_info['PLATFORM'] = os.getenv('PLATFORM')
+  key_info['VERSION'] = get_version()
 
   return jsonify(key_info)
