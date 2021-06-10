97a728df9fa4ab17f63802add87e5f60e1d38b03
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-10 10:22:02 +0100

    Added better logging for issue where version isn't accessible

--- src/aims_ui/info.py
+++ src/aims_ui/info.py
@@ -1,5 +1,4 @@
 import os
-import logging
 from . import app
 from flask import jsonify
 from importlib_metadata import version
@@ -8,7 +7,7 @@ def get_version():
   try:
     return version('aims_ui')
   except PackageNotFoundError as e:
-    logging.warning('Unable to determine aims_ui version, aims_ui was not installed as a package')
+    app.logger.warning('Unable to determine aims_ui version, aims_ui was not installed as a package')
     return 'Unknown'
 
 @app.route('/info')
