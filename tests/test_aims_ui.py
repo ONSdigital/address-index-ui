8c58733d041e5940a121392af953fb5a379a7ba6
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-10 12:13:15 +0100

    Added test for all keys in info page

--- tests/test_aims_ui.py
+++ tests/test_aims_ui.py
@@ -1,14 +1,23 @@
 import os
 import tempfile
-
+import json
 import pytest
 
 from src import aims_ui
 
 def test_info_page(client):
     """Test the info page exists, and the ENV variable is being displayed."""
+    required_keys = ['ENV','PLATFORM','VERSION']
+
     rv = client.get('/info')
-    assert b'ENV' in rv.data
+    response_dict = json.loads(rv.data)
+
+    all_keys_present = True 
+    for key in required_keys:
+      if key not in response_dict.keys():
+        all_keys_present = False
+
+    assert all_keys_present
 
 @pytest.fixture
 def client():
