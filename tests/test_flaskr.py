b0275d98bb437b4b37914708d4cce0cc1313d690
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-10 10:55:20 +0100

    Adjusted testing script to match name of project

--- tests/test_flaskr.py
+++ tests/test_flaskr.py
@@ -8,13 +8,13 @@ from src import aims_ui
 
 @pytest.fixture
 def client():
-    db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-    flaskr.app.config['TESTING'] = True
+    db_fd, aims_ui.app.config['DATABASE'] = tempfile.mkstemp()
+    aims_ui.app.config['TESTING'] = True
 
-    with flaskr.app.test_client() as client:
-        with flaskr.app.app_context():
-            flaskr.init_db()
+    with aims_ui.app.test_client() as client:
+        with aims_ui.app.app_context():
+            aims_ui.init_db()
         yield client
 
     os.close(db_fd)
-    os.unlink(flaskr.app.config['DATABASE'])
+    os.unlink(aims_ui.app.config['DATABASE'])
