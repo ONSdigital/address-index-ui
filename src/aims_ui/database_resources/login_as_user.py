6bd0d3b566d5d30d5c4d92ddbb8ea85bdf436040
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-17 17:35:57 +0100

    added explicit imports for database so no matter where the module is imported from the databaseis accessable. Made authentication run through an imported module, tidied up the code for modularity

---
+++ src/aims_ui/database_resources/login_as_user.py
@@ -0,0 +1,31 @@
+import os
+import sqlite3
+import hashlib, binascii, os
+
+def authenticate_user(input_username, input_password):
+  """Authenticate password given username"""
+
+  def get_pass(uname):
+    """Get passwordfor a user, given the username"""
+    for hashed_password in cur.execute ('SELECT password FROM USERS WHERE username=?', (uname,) )  :
+      return hashed_password[0]
+
+
+  def verify_password(stored_password, provided_password):
+    """Verify a stored password against one provided by user"""
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512', 
+                              provided_password.encode('utf-8'), 
+                              salt.encode('ascii'), 
+                              100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
+
+  con = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'auth.db'))
+  cur = con.cursor()
+
+  stored_password = get_pass(input_username)
+  con.close()
+
+  return  verify_password(stored_password, input_password)
--- src/aims_ui/login.py
+++ src/aims_ui/login.py
@@ -1,5 +1,6 @@
 import os
 from . import app
+from .database_resources.login_as_user import authenticate_user
 from flask import render_template, request
 import sqlite3
 import hashlib, binascii, os
@@ -8,32 +9,13 @@ import hashlib, binascii, os
 def login():
   if request.method == 'POST':
 
-    con = sqlite3.connect('./aims_ui/testing_resources/auth.db')
-    cur = con.cursor()
-
-    def get_pass(uname):
-      for hashed_password in cur.execute ('SELECT password FROM USERS WHERE username=?', (uname,) )  :
-        return hashed_password[0]
-
-    username = request.form.get('username')
+    input_username = request.form.get('username')
     input_password = request.form.get('password')
-    stored_password = get_pass(username)
 
-    def verify_password(stored_password, provided_password):
-      """Verify a stored password against one provided by user"""
-      salt = stored_password[:64]
-      stored_password = stored_password[64:]
-      pwdhash = hashlib.pbkdf2_hmac('sha512', 
-                                provided_password.encode('utf-8'), 
-                                salt.encode('ascii'), 
-                                100000)
-      pwdhash = binascii.hexlify(pwdhash).decode('ascii')
-      return pwdhash == stored_password
-
-    print('Password Correct?:  ' )
-    print(verify_password(stored_password, input_password))
-
-    con.close()
+    if authenticate_user(input_username, input_password):
+      print("AUTHENTICATED")
+    else:
+      print("ACCESS  DENIED FOR " + str(input_username))
 
 
     return render_template('login.html')
