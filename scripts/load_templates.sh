fa642907e1bc651f71a6f1afe19992d67e342c7a
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-06-18 19:34:48 +0100

    Finalised tempalte syncingscript to work with thie projects directooory strucuure

--- scripts/load_templates.sh
+++ scripts/load_templates.sh
@@ -10,7 +10,8 @@ TEMP_DIR=$(mktemp -d)
 
 curl -L --url "https://github.com/ONSdigital/design-system/releases/download/$DESIGN_SYSTEM_VERSION/templates.zip" --output ${TEMP_DIR}/templates.zip
 unzip ${TEMP_DIR}/templates.zip -d ${TEMP_DIR}/templates
-rm -rf app/templates/layout
-rm -rf app/templates/components
-mv ${TEMP_DIR}/templates/templates/* app/templates/
+rm -r -f src/aims_ui/templates/components
+rm -r -f src/aims_ui/templates/layout
+mkdir -p src/aims_ui/templates/components/
+mv ${TEMP_DIR}/templates/templates/* src/aims_ui/templates/components/
 rm -rf ${TEMP_DIR}
