1ccf769b3ef344350d7b449275ff7c2a4d053546
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-08-03 16:34:58 +0100

    Adjusted parameters to work in single search

--- src/aims_ui/api_interaction.py
+++ src/aims_ui/api_interaction.py
@@ -9,7 +9,7 @@ def get_params(all_user_input):
     # Do not add epoch for testing
     if (param != 'epoch') and (os.getenv('FLASK_ENV') == 'development'):
       if str(all_user_input.get(param)) != '':
-        params = params + str(param) +'='+ str(all_user_input.get(param)) + '&&'
+        params = params + str(param) +'='+ str(all_user_input.get(param)).replace('%','') + '&&'
 
   params = params[:-2] if params != '' else ''
 
@@ -21,33 +21,17 @@ def api(
     all_user_input):
   """API helper, all pages go through here to interact with API"""
 
+  header = {"Content-Type": "application/json",}
+
   if (called_from == 'uprn') or (called_from == 'postcode'):
     url = app.config.get('API_URL') + url + all_user_input.get(called_from)
-#    r = requests.get(url, )
     params = get_params(all_user_input)
-    header = {"Content-Type": "application/json",}
-
-    print(url)
-    print(all_user_input)
-    print(params, header)
     r = requests.get(url, params=params, headers=header, timeout=1000000., verify=False)
 
   elif called_from == 'singlesearch':
     url = app.config.get('API_URL') + url
-    params = {'input': all_user_input.get('input')}
-    print("")
-    print("Old params are")
-    print(params)
-
-    print("")
-    print("All user input is ")
-    print(all_user_input)
-    print("")
-    print("proposed:")
     proposed_params = {k: v for k, v in all_user_input.items() if v}
-    proposed_params['epoch'] = ''
-    print(proposed_params)
-
-    r = requests.get(url, params=params)
+    params = get_params(all_user_input)
+    r = requests.get(url, params=params, headers=header, timeout=1000000., verify=False)
 
   return r
