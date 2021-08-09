5e0fe46585e5b979427d27cac360496340dd0bde
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-08-09 17:58:24 +0100

    Added foundation for error handling on file upload

--- src/aims_ui/page_multiple_address.py
+++ src/aims_ui/page_multiple_address.py
@@ -1,6 +1,7 @@
 import os
 from flask import render_template, request, session
 from flask_login import login_required
+from werkzeug.utils import secure_filename
 from . import app
 from .cookie_utils import save_input, load_input, get_all_inputs, delete_input, load_save_store_inputs
 from .api_interaction import api
@@ -8,11 +9,11 @@ from .models.get_endpoints import get_endpoints
 from .models.get_fields import get_fields
 from .models.get_addresses import get_addresses
 import json
+import os 
 
 page_name = 'multiple_address'
 
-from werkzeug.utils import secure_filename
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'csv'}
 def allowed_file(filename):
   return '.' in filename and \
     filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -21,38 +22,42 @@ def allowed_file(filename):
 @app.route(f'/{page_name}', methods=['GET', 'POST'])
 def multiple_address():
 
+  def final(
+      error_description='', 
+      error_type = '',):
+    return render_template(
+        f'{page_name}.html',
+        error_description=error_description,
+        error_type=error_type,
+        endpoints=get_endpoints(called_from=page_name),
+        results_page=True, ) 
+
   if request.method == 'POST':
-    print('POST happening now')
-    # check if the post request has the file part
     if 'file' not in request.files:
-      print('No file part')
       return redirect(request.url)
+
     file = request.files['file']
-    print(file)
-    # If the user does not select a file, the browser submits an
-    # empty file without a filename.
+    file_size = int(os.fstat(file.fileno()).st_size) / 1000000 # In MB
+
+
     if file.filename == '':
-      print('No selected file')
-      return redirect(request.url)
+      return final(
+          error_description='Select a file that is a CSV ', 
+          error_type='file-error')
+
+    if file_size > 2:
+      return final(
+          error_description='File size is too large. Please enter a file no larger than {max_file_size}', 
+          error_type='file-size')
+
     if file and allowed_file(file.filename):
       filename = secure_filename(file.filename)
       print(file)
-      return 'Upload sucessful i think lol'
+      return final()
 
-  print('get happening')
   delete_input(session)
-
   return render_template(
       f'{page_name}.html',
       searchable_fields=get_fields(page_name),
       endpoints=get_endpoints(called_from=page_name),
-  )
-
-
-  print("POST IS HAPPENING!")
-  print(request.files)
-  return render_template(
-      f'{page_name}.html',
-      endpoints=get_endpoints(called_from=page_name),
-      results_page=True,
   )
