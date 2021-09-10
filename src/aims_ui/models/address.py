6dabf4a72f624c58a71f6f59bdc1e10845c58183
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-09-10 10:28:49 +0100

    Added classes to better  detail aims response

--- src/aims_ui/models/address.py
+++ src/aims_ui/models/address.py
@@ -2,6 +2,48 @@ import json
 from .utilities.classifications import get_classification_list
 from aims_ui import app
 
+class Pao():
+  def __init__(self,pao):
+    self.paoText = pao.get('paoText')
+    self.paoStartNumber= pao.get('paoStartNumber')
+    self.paoStartSuffix= pao.get('paoStartSuffix')
+    self.paoEndNumber= pao.get('paoEndNumber')
+    self.paoEndSuffix= pao.get('paoEndSuffix')
+
+class Sao():
+  def __init__(self,sao):
+    self.saoText = sao.get('saoText')
+    self.saoStartNumber = sao.get('saoStartNumber')
+    self.saoStartSuffix = sao.get('saoStartSuffix')
+    self.saoEndNumber = sao.get('saoEndNumber')
+    self.saoEndSuffix = sao.get('saoEndSuffix')
+
+class Nag():
+  def __init__(
+    self,
+    nag):
+    nag = nag[0]
+    
+    self.uprn = nag.get('uprn')
+    self.postcodeLocator= nag.get('postcodeLocator')
+    self.addressBasePostal= nag.get('addressBasePostal')
+    self.usrn= nag.get('usrn')
+    self.lpiKey= nag.get('lpiKey')
+    self.pao= Pao(nag.get('pao'))
+    self.sao= Sao(nag.get('sao'))
+    self.level= nag.get('level')
+    self.officialFlag= nag.get('officialFlag')
+    self.logicalStatus= nag.get('logicalStatus')
+    self.streetDescriptor= nag.get('streetDescriptor')
+    self.townName= nag.get('townName')
+    self.locality= nag.get('locality')
+    self.organisation= nag.get('organisation')
+    self.legalName= nag.get('legalName')
+    self.localCustodianCode= nag.get('localCustodianCode')
+    self.localCustodianName= nag.get('localCustodianName')
+    self.localCustodianGeogCode= nag.get('localCustodianGeogCode')
+    self.lpiEndDate= nag.get('lpiEndDate')
+    self.lpiStartDate= nag.get('lpiStartDate')
 
 class AddressAttribute():
   def __init__(
@@ -27,7 +69,6 @@ class AddressAttribute():
         'classificationCode',
         'classificationCodeList',
         'confidenceScore',
-        'nag',
     ]
 
     if name in full_values_to_show:
@@ -50,15 +91,14 @@ class AddressAttribute():
     if self.name == 'classificationCodeList':
       return get_classification_list()
     if self.name == 'nag':
-      self.name = 'localCustodianName'
-      return self.raw_value[0].get('localCustodianName') if len(
-          self.raw_value) > 0 else ''
+      self.value = Nag(self.raw_value)
 
     return f'{value}'
 
 
 class Address():
   def __init__(self, address_data):
+    print(address_data)
     # Essentially all atributes of an expected address from AIMS API (Verbose)
     self.uprn = AddressAttribute(address_data, 'uprn')
     self.formatted_address = AddressAttribute(address_data, 'formattedAddress')
