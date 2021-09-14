9827321cdaddda6533c5a2524f8046753f9960f9
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2021-09-14 09:19:05 +0100

    Adjusted so all endpoints can use the Address model, as it can now only cope with one address at a time, but also now can contain all info from the endpoint

--- src/aims_ui/models/get_addresses.py
+++ src/aims_ui/models/get_addresses.py
@@ -14,6 +14,6 @@ def get_addresses(json_response, called_from):
     address_json = response.get('addresses')
 
     for address in address_json:
-      addresses.append(Address(address))
+      addresses.append(Address({'address':address} ))
 
   return addresses
