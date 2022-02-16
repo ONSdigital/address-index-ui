3c4fdc4dbdd2189a8ba579f92fe537b43e4c3053
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-02-16 08:52:23 +0000

    Changed default landing page

--- src/aims_ui/login.py
+++ src/aims_ui/login.py
@@ -30,7 +30,7 @@ def login():
 
       login_user(user)
 
-      return redirect(url_for('uprn').replace('http://', 'https://', 1))
+      return redirect(url_for('singlesearch').replace('http://', 'https://', 1))
 
   return render_template('login.html',
                          endpoints=get_endpoints(),
