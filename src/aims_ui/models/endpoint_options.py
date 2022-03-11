759f3ee05d66d15d195e82a9cc8c8c0b4bd2c7ac
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-03-11 10:09:21 +0000

    Remvoed search button from typeahead

--- src/aims_ui/models/endpoint_options.py
+++ src/aims_ui/models/endpoint_options.py
@@ -2,5 +2,5 @@ def get_options(option):
   """Get options for a dropdown menu"""
 
   if option == 'percentage_match':
-    percent_values = [1, 2, 3, 5, 10, 25, 50]
+    percent_values = [1, 2, 3, 5, 10, 25, 50, 60, 75, 80, 95]
     return (['%' + str(x) for x in percent_values])
