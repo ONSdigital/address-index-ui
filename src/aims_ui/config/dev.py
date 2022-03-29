e40ccd1c67c8d86a93502445cf43bc6a75ec405a
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-03-29 11:07:56 +0100

    Removed confusing local URL API variables

--- src/aims_ui/config/dev.py
+++ src/aims_ui/config/dev.py
@@ -2,11 +2,4 @@
 import os
 
 SECRET_KEY = b'secretkey'
-LOCAL_API = os.getenv('LOCAL_API')
-LOCAL_API_URL = 'http://localhost:9001'
-REMOTE_API_URL = 'https://initial-test-bulk-1.aims.gcp.onsdigital.uk'
-
-if str(LOCAL_API) == 'True':
-  API_URL = LOCAL_API_URL
-else:
-  API_URL = REMOTE_API_URL
+API_URL = 'https://initial-test-bulk-1.aims.gcp.onsdigital.uk'
