a616beef40bc2529f21cca77592af1cd783ad638
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-07-04 19:59:18 +0100

    Added some error handling for multi-search, added testing code for <Response [429]> errors on that page

--- src/aims_ui/config/dev.py
+++ src/aims_ui/config/dev.py
@@ -2,5 +2,5 @@
 import os
 
 SECRET_KEY = b'secretkey'
-API_URL = 'https://analysis-prod-aims-bulk-1.gcp.onsdigital.uk'
 API_URL = 'http://localhost:9001'
+API_URL = 'https://analysis-prod-aims-bulk-1.gcp.onsdigital.uk'
