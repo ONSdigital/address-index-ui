584379f15d12313fd61dcf1f6a991b74f8a10752
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-07-04 18:28:44 +0100

    Tweaked caching so that it's possible to whitelist elements from caching using classes

--- src/aims_ui/models/get_fields.py
+++ src/aims_ui/models/get_fields.py
@@ -221,7 +221,7 @@ def get_fields(endpoint_name, include_UPRN_redirect=False):
         Field(
             'input',
             display_title='Enter search string',
-            classes='ons-input--w-40',
+            classes='ons-input--w-40 nocache',
             required=True,
             description=
             'Specifies the address search string (e.g. "14 Acacia Avenue, Ruislip, HA4 8RG").'
