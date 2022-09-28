927298627e3509115259e14cd9cc0fcaf4ab339d
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-09-28 03:03:40 +0100

    Hooked up all user endpoint and tested with UI

--- src/aims_ui/page_multiple_address_results.py
+++ src/aims_ui/page_multiple_address_results.py
@@ -19,18 +19,29 @@ def multiple_address_results():
   for endpoint in endpoints:
     endpoint.current_selected_endpoint = url_for(page_name)
 
-  headers = ['JOBID','STATUS', 'RECS PROCESSED','DOWNLOAD LINK']
-  results = all_jobs()
-  print(results.json())
-  #TODO remove testing only
-  results = [
-      ['34', '10,000 of 67,924', 'in-progress', '-'],
-      ['80', '30,501 of 80,924', 'in-progress', '-'],
-      ['212', '23 of 924', 'in-progress', '-'],
-      ['4', '40,000 of 40,000', 'complete', 'http://www.example.com'],
-      ]
+  headers = ['JOBID','STATUS', 'USER ID', 'RECS PROCESSED','DOWNLOAD LINK']
+  results = all_jobs().json().get('jobs',[])
+  formatted_results = [
+      [x.get('jobid'),
+       x.get('status'),
+       x.get('userid'),
+       f"{x.get('recssofar')}  of  {x.get('totalrecs')}",
+       x.get('a', '-')
+        ] 
+      for x in results]
+
+  print(formatted_results )
+
+  # EXAMPLE results format
+  # results = [
+  #   ['34', '10,000 of 67,924', 'in-progress', '-'],
+  #   ['80', '30,501 of 80,924', 'in-progress', '-'],
+  #   ['212', '23 of 924', 'in-progress', '-'],
+  #   ['4', '40,000 of 40,000', 'complete', 'http://www.example.com'],
+  #   ]
+
   jobs = []
-  jobs = create_table(headers, results)
+  jobs = create_table(headers, formatted_results)
 
   return render_template(
       f'{page_name}.html',
