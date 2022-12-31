38bc63a75aa8e8459a49fc1c273727dd664766f1
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-12-31 19:38:12 +0000

    Added settings page, removed redundant 'about' page, linked js to settings page

--- src/aims_ui/page_settings.py
+++ src/aims_ui/page_settings.py
@@ -1,4 +1,3 @@
-import os
 from . import app
 from flask import render_template
 from flask_login import login_required
@@ -6,7 +5,7 @@ from .models.get_endpoints import get_endpoints
 
 
 @login_required
-@app.route('/about')
-def about():
+@app.route('/settings')
+def settings():
 
-  return render_template('about.html', endpoints=get_endpoints())
+  return render_template('settings.html', endpoints=get_endpoints())
---
+++ src/aims_ui/static/custom_js/page_settings.js
@@ -0,0 +1,2 @@
+
+console.log('This works, is loaded, etc');
