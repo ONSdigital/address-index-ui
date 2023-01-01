d5c2f99be49e9c054a9a1fdea684a31328681a8b
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-01-01 21:24:26 +0000

    Added settings page to allow current preference to be preselected on the radio choice

--- src/aims_ui/static/src/page_settings.mjs
+++ src/aims_ui/static/src/page_settings.mjs
@@ -1,9 +1,21 @@
-import { updateAddressFormatPrefference } from './local_storage_helpers.mjs';
-console.log('ahhh, is loaded, etc');
-
+import { 
+  getAddressTitlePrefference, 
+  updateAddressFormatPrefference 
+} from './local_storage_helpers.mjs';
 
 function updateAddressTitlePrefference(e) {
   updateAddressFormatPrefference(e);
+}
+
+function setupNagAndPafStatus() {
+  const current_status = getAddressTitlePrefference();
+  if (current_status === 'paf') {
+    const pafRadio = document.querySelector('#paf-radio');
+    pafRadio.checked = true;
+  } else if (current_status === 'nag') {
+    const nagRadio = document.querySelector('#nag-radio');
+    nagRadio.checked = true;
+  }
 }
 
 function setupNagAndPafListeners() {
@@ -17,13 +29,15 @@ function setupNagAndPafListeners() {
   nagRadio.addEventListener('change', (e) => {
     updateAddressTitlePrefference('nag');
   });
-
 }
+
+
 
 
 
 function init() {
   setupNagAndPafListeners()
+  setupNagAndPafStatus()
 }
 
 window.addEventListener('load', init);
