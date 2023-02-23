75851907612ab165e728b7e93a66e48bc0ad60ee
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-02-23 09:17:29 +0000

    Fixed signout bug

--- src/aims_ui/__init__.py
+++ src/aims_ui/__init__.py
@@ -138,6 +138,7 @@ def get_cached_tooltip_data():
 
 from . import info
 from . import login
+from . import signout
 from . import page_settings
 from . import page_uprn
 from . import page_postcode
