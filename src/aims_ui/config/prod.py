b8668334418792c83c8d5deb44009f8feb4cb8c7
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-12-04 15:38:06 +0000

    debug

--- src/aims_ui/config/prod.py
+++ src/aims_ui/config/prod.py
@@ -8,3 +8,7 @@ if not (SECRET_KEY := os.getenv('SECRET_KEY')):
   raise RuntimeError('no SECRET_KEY ENV variable set')
 
 JWT_K_VALUE = str(os.getenv('jwt_k_value'))
+print()
+print('K VALUE is ')
+print(JWT_K_VALUE)
+print()
