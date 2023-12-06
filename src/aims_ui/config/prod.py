43fe62689fbef177f555ec043ac9536def850145
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-12-06 07:44:21 +0000

    Capitalised variables

--- src/aims_ui/config/prod.py
+++ src/aims_ui/config/prod.py
@@ -8,7 +8,7 @@ if not (API_URL := os.getenv('API_URL')):
 if not (SECRET_KEY := os.getenv('SECRET_KEY')):
   raise RuntimeError('no SECRET_KEY ENV variable set')
 
-JWT_K_VALUE = str(os.getenv('jwt_k_value'))
+JWT_K_VALUE = str(os.getenv('JWT_K_VALUE'))
 
 logging.info(' \n\n KVALUE is: ' + JWT_K_VALUE)
-logging.info(' \n\n OS  ---------- KVALUE is: ' + str(os.getenv('jwt_k_value')))
+logging.info(' \n\n OS  ---------- KVALUE is: ' + str(os.getenv('JWT_K_VALUE')))
