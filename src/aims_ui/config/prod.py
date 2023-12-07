b9db893827d0d45a57cb35be10b61563d8eb7627
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-12-07 12:34:38 +0000

    Added b64 decode for kvalue secret

--- src/aims_ui/config/prod.py
+++ src/aims_ui/config/prod.py
@@ -1,6 +1,7 @@
 """FLASK PROD CONFIG"""
 import os
 import logging
+import base64
 
 if not (API_URL := os.getenv('API_URL')):
   raise RuntimeError('no API_URL ENV variable set')
@@ -9,6 +10,5 @@ if not (SECRET_KEY := os.getenv('SECRET_KEY')):
   raise RuntimeError('no SECRET_KEY ENV variable set')
 
 JWT_K_VALUE = str(os.getenv('JWT_K_VALUE'))
+JWT_K_VALUE = base64.b64decode(JWT_K_VALUE)
 
-logging.info(' \n\n KVALUE is: ' + JWT_K_VALUE)
-logging.info(' \n\n OS  ---------- KVALUE is: ' + str(os.getenv('JWT_K_VALUE')))
