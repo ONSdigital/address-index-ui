830e3ccf2235a924089e5a28b4d3248800ec019c
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-12-11 14:15:36 +0000

    Adjusted auth checker to handle labels in the job id

--- src/aims_ui/download_handler.py
+++ src/aims_ui/download_handler.py
@@ -7,6 +7,7 @@ from io import StringIO, BytesIO
 from flask import render_template, request, session, send_file
 from flask_login import login_required
 from .multiple_address_utils import job_url_if_authorised
+import logging
 
 # For the gz download
 import urllib.request
