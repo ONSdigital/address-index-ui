7cc8868f2d550816ca16cc861a5d1198ce777ede
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-05-22 09:40:46 +0100

    Moved utils into more approprite 'page_helpers' and nested an 'api' helpers folder

--- src/aims_ui/page_controllers/1-single_matches/page_postcode.py
+++ src/aims_ui/page_controllers/1-single_matches/page_postcode.py
@@ -3,7 +3,7 @@ from flask import render_template, request, session
 from flask_login import login_required
 from . import app
 from requests.exceptions import ConnectionError
-from .cookie_utils import save_input, load_input, get_all_inputs, delete_input, load_save_store_inputs, save_epoch_number
+from aims_ui.page_helpers.cookie_utils import save_input, load_input, get_all_inputs, delete_input, load_save_store_inputs, save_epoch_number
 from .api_interaction import api
 from .security_utils import detect_xml_injection, check_user_has_access_to_page
 from .models.get_endpoints import get_endpoints
