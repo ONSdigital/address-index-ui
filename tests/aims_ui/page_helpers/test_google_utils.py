6e76a777037d457e507f5911eefb1d784eb2cfd4
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-06-06 19:32:26 +0100

    Removed failing test

--- tests/aims_ui/page_helpers/test_google_utils.py
+++ tests/aims_ui/page_helpers/test_google_utils.py
@@ -48,10 +48,3 @@ def test_current_group():
     group = get_current_group()
     assert group.get('name') == 'default'
 
-def test_FAIL():
-  """ this tet should fail, to test the CI/CD pipeline """
-
-  i_am_a_sad_test = False
-
-  assert i_am_a_sad_test == True
-
