ea2c02ba15f41b0f912c33436a01508b00d1c3f9
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-06-10 16:53:13 +0100

    Added postcode js test

--- src/aims_ui/static/js/a_single_matches/postcode.mjs
+++ src/aims_ui/static/js/a_single_matches/postcode.mjs
@@ -1,11 +1,11 @@
-function forceCapsInput() {
+export function forceCapsInput() {
   const postcodeInput = document.querySelector('#postcode');
   postcodeInput.addEventListener('input', (e) => {
     e.target.value = e.target.value.toUpperCase();
   });
 }
 
-function init() {
+export function init() {
   console.log('postcode specific loaded');
   forceCapsInput();
 }
