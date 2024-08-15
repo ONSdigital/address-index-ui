9e016bf7415ffd82742f1ffa7b60eace563941c1
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-08-15 10:27:10 +0100

    Changed Gconf to disable tls1 and 1.1

--- gconf.py
+++ gconf.py
@@ -6,8 +6,13 @@ PORT = int(os.getenv("PORT", 5000))
 
 # Gunicorn config
 bind = ":" + str(PORT)
-# Disable TLSv1.0 and TLSv1.1
-ssl_version = 'TLSv1_2'
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2 * multiprocessing.cpu_count()
 timeout = 420
+
+# Disable TLSv1.0 and TLSv1.1
+def ssl_context(conf, default_ssl_context_factory):
+    import ssl
+    context = default_ssl_context_factory()
+    context.minimum_version = ssl.TLSVersion.TLSv1_2
+    return context
