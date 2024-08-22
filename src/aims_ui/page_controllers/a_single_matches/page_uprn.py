839d4e2108a0f3d7ecaf7b88027f33e44b07a2e6
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-08-22 12:51:23 +0100

    Updated UPRN page to use new errors

--- src/aims_ui/page_controllers/a_single_matches/page_uprn.py
+++ src/aims_ui/page_controllers/a_single_matches/page_uprn.py
@@ -6,6 +6,7 @@ from aims_ui.page_helpers.cookie_utils import delete_input, load_save_store_inpu
 from aims_ui.page_helpers.api.api_interaction import api
 from aims_ui.page_helpers.security_utils import detect_xml_injection, check_user_has_access_to_page
 from aims_ui.page_helpers.pages_location_utils import get_page_location
+from aims_ui.page_helpers.error.error_utils import error_page_xml, error_page_api_request, error_page_api_response
 from aims_ui.models.get_endpoints import get_endpoints
 from aims_ui.models.get_fields import get_fields
 from aims_ui.models.get_addresses import get_addresses
@@ -43,11 +44,8 @@ def uprn():
   user_input = all_user_input.get('uprn', '')
   xml_injection = detect_xml_injection(user_input)
   if xml_injection:
-    return page_error(None,
-                      page_name,
-                      all_user_input,
-                      override_error_description=
-                      'XML Attack Detected. This incident will be reported.')
+    return error_page_xml(page_name, user_input)
+
 
   try:
     result = api(
@@ -56,16 +54,15 @@ def uprn():
         all_user_input,
     )
 
-  except ConnectionError as e:
-    return page_error(None, e, page_name)
+  # Deal with errors connecting to the API
+  except Exception as e:
+    return error_page_api_request(page_name, user_input, e)
 
-  if result.status_code == 200:
-    matched_addresses = get_addresses(result.json(), page_name)
-  elif result.status_code == 404:
-    # No results but the api compelted the call successfully
-    matched_addresses = ''
-  else:
-    return page_error(result, page_name)
+  # Errors after sucessful Response
+  if result.status_code != 200:
+    return error_page_api_response(page_name, user_input, result)
+
+  matched_addresses = get_addresses(result.json(), page_name)
 
   # Save epoch number
   save_epoch_number(session, all_user_input.get('epoch', ''))
