f4a833546c818e1dae3b1f196cfaa9a3b9178d35
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-11-13 16:57:22 +0000

    Removed unneeded utility file

--- src/aims_ui/page_helpers/error/multiple_match_error.py
+++
@@ -1,5 +0,0 @@
-from aims_ui.page_controllers.f_error_pages.page_error import page_error
-from aims_ui.page_helpers.google_utils import get_username
-from requests.exceptions import ConnectionError, Timeout
-import logging
-""" Handle decisions for logging and presenting errors """
