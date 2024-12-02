ae2184cce70f035c277e0c22fcdf19667c6509dc
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2024-12-02 08:06:32 +0000

    Revert change to enabled loading saving and analysis of all inputs for the typeahead dynamically updating

--- src/aims_ui/static/js/a_single_matches/typeahead.mjs
+++ src/aims_ui/static/js/a_single_matches/typeahead.mjs
@@ -1,7 +1,7 @@
 
 function getAllInputs() {
   const matchForm = document.querySelector('.match-form-container');
-  const inputs = matchForm.querySelectorAll('input:not([type="radio"]):not([type="checkbox"])');
+  const inputs = matchForm.querySelectorAll('input');
   return inputs
 }
 
