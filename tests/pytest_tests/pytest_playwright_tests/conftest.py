dca2bb7c59f3f0426e3fc1f1df4c06dc631f5ee5
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2025-01-17 20:11:39 +0000

    Refactored header tests to use a login and goto fixture, enabling much cleaner addition of more tests - in this case ensuring the footer is available for each page for each user. Other tests for all pages can be added using the class structure and goto fixture

--- tests/pytest_tests/pytest_playwright_tests/conftest.py
+++ tests/pytest_tests/pytest_playwright_tests/conftest.py
@@ -1,6 +1,10 @@
 """ Special Configuration File for Pytest Playwright Tests """
 
 import pytest
+from playwright.sync_api import Page
+
+from tests.pytest_tests.pytest_playwright_tests.utils.constants import BASE_URL, get_page_url_from_page_name
+from tests.pytest_tests.pytest_playwright_tests.utils.setup_helpers import login_as_role
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -8,3 +12,17 @@ def global_setup_available_everywhere():
   print('Global setup run ONCE for all tests')
 
   # Check here that there's a version of the API that's accessible to the UI
+
+
+@pytest.fixture
+def login_and_goto(page: Page):
+  """ Fixture to login as a user role and go to a page """
+
+  def _login_and_goto(user_role: str, page_name: str):
+    page.context.clear_cookies()
+    login_as_role(page, user_role)
+    page_url = get_page_url_from_page_name(page_name)
+    page.goto(f"{BASE_URL}{page_url}")
+    return page
+
+  return _login_and_goto
--- tests/pytest_tests/pytest_playwright_tests/test_all_pages.py
+++ tests/pytest_tests/pytest_playwright_tests/test_all_pages.py
@@ -3,9 +3,12 @@ import pytest
 from playwright.sync_api import Page, expect
 
 from tests.pytest_tests.pytest_playwright_tests.utils.constants import (
-    ALL_PAGE_NAMES, BASE_URL, ROLES, get_just_header_pages,
-    get_page_url_from_page_name, role_to_username)
-from tests.pytest_tests.pytest_playwright_tests.utils.setup_helpers import login_as_role
+    ALL_PAGE_NAMES,
+    BASE_URL,
+    ROLES,
+    get_just_header_pages,
+    role_to_username
+)
 
 
 def test_title_tag(page: Page):
@@ -20,44 +23,50 @@ def test_header_visible(page: Page):
   expect(page.locator('header')).to_be_visible()
 
 
-@pytest.mark.parametrize('user_role', ROLES)
-def test_available_header_pages(page: Page, user_role: str):
-  """ Test that the header contains the correct navigation links for each role """
+@pytest.mark.parametrize("user_role", ROLES)
+@pytest.mark.parametrize("page_name", ALL_PAGE_NAMES)
+class TestPageElements:
+  """Test various elements (header, footer, etc.) on each page for each role."""
 
-  page.context.clear_cookies()
+  def test_header_is_visible_and_has_correct_links(self, page: Page,
+                                                   user_role: str,
+                                                   page_name: str,
+                                                   login_and_goto):
+    page = login_and_goto(user_role, page_name)
 
-  # Login with a username matching the role and return username
-  username = login_as_role(page, user_role)
-
-  # For every page (including ones that *might* be inaccesible)
-  for page_name in ALL_PAGE_NAMES:
-    page_url = get_page_url_from_page_name(page_name)
-    testing_page_uri = f'{BASE_URL}{page_url}'
-    page.goto(testing_page_uri)
-
-    # Check the header is visible (even for inaccessible pages)
+    # HEADER checks
     expect(page.locator('header')).to_be_visible()
-
-    # Get all allowed pages for the current role
     user_info_for_role = role_to_username(user_role)
     allowed_pages_info = user_info_for_role.get('allowed_pages_info')
-
-    # Remove any pages that are subpages from the header check
     expected_header_pages = get_just_header_pages(allowed_pages_info)
 
     print(
-        f'\n FOR THE USERNAME "{username}", with ROLE {user_role}, the EXPECTED HEADER PAGES are: {[ "name: " + x.get("page_name") + " url:" + x.get("url") for x in expected_header_pages]}\n\n'
+        f'\n FOR THE USERNAME "{user_info_for_role.get("username")}",'
+        f' with ROLE {user_role},'
+        f' the EXPECTED HEADER PAGES are:'
+        f' {[ "  name: " + x.get("page_name") + "\n    url:" + x.get("url") for x in expected_header_pages]}\n'
     )
 
-    # Loop over all expected pages, check for link in header
     for allowed_page in expected_header_pages:
       link_element = page.locator('.ons-navigation__link').filter(
           has_text=allowed_page.get('page_name'))
-      expected_page_uri = f'/{allowed_page.get("url")}'
+      expected_page_uri = f"/{allowed_page.get('url')}"
+      print(f'   Checking link: {allowed_page.get("page_name")}'
+            f' should have url: {expected_page_uri}')
+      print(
+          'Check that the last tested link *should be visible* for the user role.'
+      )
 
-      actual_link_element_uri = link_element.get_attribute('href')
-      if actual_link_element_uri != expected_page_uri:
-        print(
-            f'\n Checking link element: {link_element} \n to have expected uri: "{expected_page_uri}"\n it actually has: "{actual_link_element_uri}"\n\n'
-        )
       expect(link_element).to_have_attribute('href', expected_page_uri)
+
+  def test_footer_is_visible_and_correct(self, page: Page, user_role: str,
+                                         page_name: str, login_and_goto):
+    page = login_and_goto(user_role, page_name)
+
+    # FOOTER checks
+    footer = page.locator('footer')
+    expect(footer).to_be_visible()
+
+    footer_help_link = footer.get_by_text(
+        'Have feedback or found an issue with matched address(es)?')
+    expect(footer_help_link).to_have_attribute('href', '/help/submit_feedback')
