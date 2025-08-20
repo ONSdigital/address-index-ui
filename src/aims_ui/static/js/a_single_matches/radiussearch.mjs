0dbc8a788815a9a2c131ef9916612d43831e13c8
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2025-08-20 18:21:58 +0100

    Reorganised page with CSS to have the parameters appear in a horizontal manner, the map and results all available on one page. Next steps: Make all of the inputs by default be invisible until the js re-organises them - so that the user doesn't see the jump Make the Lat/Long availabable in JS feed into the inputs for the rest of the page and trigger a search Make the results section have a toggle for 'compressed view' allowing results to be shown in a table format like in the original demo Make the results returned appear on the Map with markers If a user enters a lat/long then update the map including with a circle of the current 'range' Add css around the map using the design system style guidance so that it doesn't appear out of place

--- src/aims_ui/static/js/a_single_matches/radiussearch.mjs
+++ src/aims_ui/static/js/a_single_matches/radiussearch.mjs
@@ -1,3 +1,34 @@
+
+function setupHorizontalInputs() {
+  // Select all of the class ons-field inside the class of match-form-container
+  const inputContainer = document.querySelector('.match-form-container');
+  const fields = inputContainer.querySelectorAll('.ons-field');
+  const buttons = inputContainer.querySelectorAll('.ons-btn');
+
+  // Remove the 'br' elements
+  const brElements = inputContainer.querySelectorAll('br');
+  brElements.forEach(br => {
+    br.remove();
+  });
+
+  // Create the container
+  const horizontalContainer = document.createElement('div');
+  horizontalContainer.id = 'horizontalContainer';
+
+  fields.forEach(field => {
+    horizontalContainer.appendChild(field);
+  });
+
+  // Group buttons
+  const newButtonContainer = document.createElement('div');
+  buttons.forEach(button => {
+    newButtonContainer.appendChild(button);
+  });
+  horizontalContainer.appendChild(newButtonContainer);
+  inputContainer.appendChild(horizontalContainer);
+
+}
+
 function setupMap() {
   const INITIAL_LAT  = 51.566322;
   const INITIAL_LNG  = -3.0272245;
@@ -39,6 +70,8 @@ function setupMap() {
 function init() {
   console.log('RADIUS SEARCH specific scripts loaded');
   setupMap();
+  // Take the generated Design System components and arrange them horizontally
+  setupHorizontalInputs();
 }
 
 window.addEventListener('load', init);
