fff6f8b0bfb669b5767ca91e712f88b1ab57da68
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2025-09-10 10:17:23 +0100

    Handle the submission of a form via the enter button on inputs

--- src/aims_ui/static/js/a_single_matches/radiussearch/form_helpers.mjs
+++ src/aims_ui/static/js/a_single_matches/radiussearch/form_helpers.mjs
@@ -14,9 +14,34 @@ function preventUprnButtonSubmittingForm() {
   });
 }
 
+function addListenersForEnterKeyToSubmitForm() {
+  // Deffine all the input element ids that should submit the form when "Enter" is pressed
+  const inputIds = ['lat', 'lon', 'rangekm', 'classificationfilter', 'input', 'limit'];
+
+  // Get the parent which contains all input elements
+  const inputContainer = document.querySelector('#full-match-form-container');
+
+  // For each input id, add a listener to submit the form when "Enter" is pressed
+  for (const id of inputIds) {
+    // Get each element
+    const inputElement = inputContainer.querySelector('#' + id);
+    // Add an event listener for all keys
+    inputElement.addEventListener('keydown', (e) => {
+      // If the key is Enter
+      if (e.key === 'Enter') {
+        e.preventDefault();
+        // Submit the form
+        const form = inputElement.closest('form');
+        form.requestSubmit();
+      }
+    });
+  };
+}
+
 export function setupSubmissionFromInputs() {
   // Firstly stop the UPRN search button from submitting raidus search
   preventUprnButtonSubmittingForm();
 
   // Now add listeners to all other inputs to submit the form when "Enter" is pressed
+  addListenersForEnterKeyToSubmitForm();
 }
