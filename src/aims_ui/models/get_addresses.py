85f4f57900421dc6b5be018245810df1859fdac8
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2025-11-03 13:23:36 +0000

    Changed the default value to True for UPRN lookups, as this inadvertently caused the address info page to not include hierarchy information

--- src/aims_ui/models/get_addresses.py
+++ src/aims_ui/models/get_addresses.py
@@ -10,7 +10,8 @@ def get_addresses(json_response,
   addresses = []
 
   if called_from == 'uprn':
-    response = (json_response.get('response'))
+    response = json_response.get('response')
+    print('Response for UPRN address:', response)
     addresses = [
         Address(response,
                 include_hierarchy=True,
