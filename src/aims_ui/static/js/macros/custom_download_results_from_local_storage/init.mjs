8b3b21963bdb7ca1207256401e6cacc3ac993cd0
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2025-11-04 18:02:14 +0000

    Added download button to the Radius Search Page

---
+++ src/aims_ui/static/js/macros/custom_download_results_from_local_storage/init.mjs
@@ -0,0 +1,33 @@
+// Initialise listeners and functionality to allow the download button to 
+// download addresses from local storage
+
+import { getPageLocalValues } from "/static/js/f_helpers/local_storage_page_helpers.mjs";
+import { downloadCsvFromLocalStorage } from "/static/js/macros/custom_download_results_from_local_storage/download_helpers.mjs";
+
+function getTheDownloadButton() {
+  return document.querySelector('#download-address-results-button');  
+}
+
+function setupDownloadButtonListeners(page_name) {
+  const downloadButton = getTheDownloadButton();
+
+  downloadButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    downloadCsvFromLocalStorage(page_name);
+  });
+}
+
+function makeVisibleIfThereAreResults() {
+  // Get a handle on the container
+  const container = document.querySelector('#container-for-download-address-results-button');
+  container.classList.remove('invisible');
+}
+
+export function init(page_name) {
+  const localPageValues = getPageLocalValues(page_name);
+  const previousAddresses = localPageValues.mostRecentlySearchedAddresses || [];
+  if (previousAddresses.length > 0) {
+    setupDownloadButtonListeners(page_name);
+    makeVisibleIfThereAreResults(previousAddresses);
+  }
+}
