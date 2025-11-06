94c3dd04b2cbaa00053e9fa7c9979cf4c4c44d79
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2025-11-06 18:28:44 +0000

    Added groundwork for the clear button clearing the new, persistant address cards. Will require finishing and then deploying to other pages

---
+++ src/aims_ui/static/js/macros/custom_search/clear_button.mjs
@@ -0,0 +1,14 @@
+import { getPageLocalValues } from "/static/js/f_helpers/local_storage_page_helpers.mjs";
+import { crEl } from "/static/js/f_helpers/element_creation.mjs";
+
+export function setupClearButton(page_name) {
+  // Get a handle on the clear button
+
+  // Prevent default action of visiting a link
+
+  // Add the action to reset the 'page previously searched values' in local storage 
+
+  // Hide or delete/refresh the results message and table
+
+  console.log('Setting up clear button for page:', page_name);
+}
