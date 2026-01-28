eaa9e12643d717136ce3f47523b1141df7833ae0
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2026-01-28 21:27:02 +0000

    Remove last of outdated 'setup defaults' for the new default values

--- src/aims_ui/static/js/e_all_pages/all_pages_first_js.mjs
+++ src/aims_ui/static/js/e_all_pages/all_pages_first_js.mjs
@@ -1,17 +1,5 @@
-import {
-  setDefaultResponseFormatCustomResponse,
-} from '/static/js/f_helpers/local_storage_helpers.mjs';
-
 import { setupDefaultGlobalValues } from '/static/js/e_all_pages/setup_defaults.mjs';
-
 import { addMostRecentPageToStorage } from '/static/js/e_all_pages/breadcrumb_helpers/breadcrumbHelpers.mjs';
-
-
-function setDefaultValuesIfUnset() {
-  setDefaultResponseFormatCustomResponse();
-
-  setupDefaultGlobalValues();
-}
 
 function removeResubmissionMessage() {
   if (window.history.replaceState) {
@@ -21,7 +9,7 @@ function removeResubmissionMessage() {
 
 function init() {
   console.log('all_pages_first loaded');
-  setDefaultValuesIfUnset();
+  setupDefaultGlobalValues();
   addMostRecentPageToStorage();
   removeResubmissionMessage();
 }
