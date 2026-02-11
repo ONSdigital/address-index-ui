917756d5b025bbfb9928adc976ac2907bfb88a5a
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2026-02-11 15:26:19 +0000

    Added reset functionality

--- src/aims_ui/static/js/d_misc_functionality/page_settings/reset_settings_button.mjs
+++ src/aims_ui/static/js/d_misc_functionality/page_settings/reset_settings_button.mjs
@@ -1,9 +1,18 @@
+import { getDefaultGlobalValues } from '/static/js/e_all_pages/default_helpers/default_values.mjs';
 import {
-  getGlobalValues,
   setGlobalValues,
 } from '/static/js/f_helpers/local_storage_page_helpers.mjs';
 
 // Setup listeners and functionality for the reset settings button
+
+function resetSettingsToDefaultValues() {
+  // Get the default global settings
+  const defaultGlobalValues = getDefaultGlobalValues();
+  console.debug('Resetting settings to default values. Default global values to be set:', defaultGlobalValues);
+
+  // Set the global values as the default values
+  setGlobalValues(defaultGlobalValues);
+}
 
 function getResetSettingsContainer() {
   const resetContainer = document.querySelector('#reset-settings-container');
@@ -53,10 +62,32 @@ export function setupResetSettingsButton() {
   confirmationInput.addEventListener('keyup', () => {
     const isContentReset = confirmationInput.value === 'RESET';
     if (isContentReset) {
+      resetButton.disabled = false;
       resetButton.classList.remove('ons-btn--disabled');
     } else {
+      resetButton.disabled = true;
       resetButton.classList.add('ons-btn--disabled');
     }
   });
 
+  // Add click listener to reset button
+  resetButton.addEventListener('click', () => {
+    console.log('Reset settings button clicked. Checking confirmation input and button state before resetting settings.');
+    if (confirmationInput.value !== 'RESET') {
+      console.warn('Reset settings button clicked but confirmation input does not match "RESET". Action will not be performed.');
+      return;
+    }
+
+    // Check the css for the button doesnt have ons-btn--disabled
+    if (resetButton.classList.contains('ons-btn--disabled')) {
+      console.warn('Reset settings button clicked but button is disabled. Action will not be performed.');
+      return;
+    }
+
+    // Run the reset local storage script, refresh page
+    resetSettingsToDefaultValues();
+
+    // Refresh the page to apply the default settings
+    location.reload();
+  });
 }
