f44a665493b9544292c18632cd7d0c52cbe6340d
Felixim0 <44896994+Felixim0@users.noreply.github.com>
2026-02-24 13:36:25 +0000

    Working but not very robust or new. Relies on depricated methods. Will have to think about perhaps refactoring how this one works...

--- src/aims_ui/page_controllers/d_misc_functionality/download_handlers/small_multiple_address_download.py
+++ src/aims_ui/page_controllers/d_misc_functionality/download_handlers/small_multiple_address_download.py
@@ -1,37 +1,101 @@
-import csv
-import json
-import os
-from io import BytesIO, StringIO
 
-import requests
 from flask import request, send_file
 from flask_login import login_required
 
+from flask import render_template, request, send_file, session
+from flask_login import login_required
+
 from aims_ui import app
-from aims_ui.app_helpers.classification_utils.autosuggest import get_classification_autosuggest_list, get_full_classification_autosuggest, get_classification_autosuggest_list_reversed
-from aims_ui.page_controllers.b_multiple_matches.utils.multiple_match_api_utils import job_url_if_authorised
-from aims_ui.page_controllers.f_error_pages.page_error import page_error
+from aims_ui.models.get_endpoints import get_endpoints
+from aims_ui.models.get_fields import get_fields
+from aims_ui.page_controllers.f_error_pages.page_error_annotation_multiple import page_error_annotation_multiple
+from aims_ui.page_helpers.cookie_utils import delete_input, load_save_store_inputs
+from aims_ui.page_helpers.google_utils import get_current_group
+from aims_ui.page_helpers.pages_location_utils import get_page_location
+from aims_ui.page_helpers.security_utils import check_user_has_access_to_page
 
-# For the gz download
+from aims_ui.page_controllers.b_multiple_matches.utils.multiple_match_file_upload_utils import check_valid_upload, validate_limit_parameter
+from aims_ui.page_controllers.b_multiple_matches.utils.submit_multiple_match_from_singlesearch import (
+    multiple_address_match_from_singlesearch_display,
+    multiple_address_match_from_singlesearch_download)
+from aims_ui.page_controllers.b_multiple_matches.utils.multiple_match_utils import get_results_display_type
 
-page_name = 'download_handler'
 
-print('askjdhskj')
+page_name = 'multiple_address_original'
 
 
 @login_required
 @app.route(f'/downloads/small_multiple_match', methods=['POST'])
 def small_multiple_address_download_handler():
-  # Following body was posted:
+  endpoints = get_endpoints(called_from=page_name)
+  access = check_user_has_access_to_page(page_name)
+  if access != True:
+    return access
+  page_location = get_page_location(endpoints, page_name)
 
-  form_data = request.form
-  print()
-  print()
-  print()
-  print('aslkhdkjshkjh')
-  print(form_data)
-  print()
-  print()
-  print()
-  print()
+  current_group = get_current_group()
+  bulk_limits = current_group.get('bulk_limits')
+  limit_mini_bulk = bulk_limits.get('limit_mini_bulk')
+  searchable_fields = get_fields(page_name)
 
+  try:
+    all_user_input = load_save_store_inputs(
+        searchable_fields,
+        request,
+        session,
+    )
+    # Throw and exception for issues with the 'limit' parameter
+    validate_limit_parameter(all_user_input)
+  except Exception as e:
+    # Particularly to handle files that are too large
+    return page_error_annotation_multiple(page_name, {}, e)
+
+  file = request.files['file_upload']
+
+  try:
+    file_valid, error_description, error_title = check_valid_upload(
+        file, limit_mini_bulk)
+  # File Upload issues appear inside upload component
+  except Exception as e:
+    return page_error_annotation_multiple(page_name, all_user_input,
+                                          e.error_description)
+
+  if not file_valid:
+    return page_error_annotation_multiple(page_name, all_user_input,
+                                          error_description)
+
+  display_type = get_results_display_type(searchable_fields)
+
+  if display_type == 'Download':
+    try:
+      full_results, line_count = multiple_address_match_from_singlesearch_download(
+          file, all_user_input)
+
+      return send_file(full_results,
+                       mimetype='text/csv',
+                       download_name=f'result_size_{line_count}.csv',
+                       as_attachment=True)
+
+    except Exception as e:
+      return page_error_annotation_multiple(page_name, all_user_input, e)
+
+  elif display_type == 'Display':
+    try:
+      table_results, results_summary_table = multiple_address_match_from_singlesearch_display(
+          file, all_user_input)
+
+      return render_template(
+          page_location,
+          page_name=page_name,
+          endpoints=endpoints,
+          error_description=error_description,
+          error_title=error_title,
+          searchable_fields=searchable_fields,
+          table_results=table_results,
+          results_summary_table=results_summary_table,
+          results_page=True,
+          bulk_limits=bulk_limits,
+      )
+
+    except Exception as e:
+      return page_error_annotation_multiple(page_name, all_user_input, e)
--- src/aims_ui/static/js/b_multiple_matches/multiple_address_original/setup_load_button.mjs
+++ src/aims_ui/static/js/b_multiple_matches/multiple_address_original/setup_load_button.mjs
@@ -1,6 +1,14 @@
 
 const isLoadingClassName = 'ons-is-loading';
 
+function removeLoadingClass(loadButton) {
+  loadButton.classList.remove(isLoadingClassName);
+}
+
+function addLoadingClass(loadButton) {
+  loadButton.classList.add(isLoadingClassName);
+}
+
 export function setupLoadButtonInit() {
   const loadButton = document.querySelector('#downloadButtonMultipleMatch');
 
@@ -9,7 +17,7 @@ export function setupLoadButtonInit() {
     event.preventDefault();
 
     // Add the loading class name
-    loadButton.classList.add(isLoadingClassName);
+    addLoadingClass(loadButton);
 
     // Submit a post request to this url when the button is clicked
     const response = await fetch('/downloads/small_multiple_match', {
@@ -17,26 +25,42 @@ export function setupLoadButtonInit() {
         body: new FormData(document.querySelector('form'))
     });
 
-    // If the response is not ok, submit the entire form as normal to trigger the default errors
-    if (!response.ok) {
-      console.error('There was an error starting the download:', response);
-      document.querySelector('form').submit();
-      return;
+    // Detect if it's a csv file or html
+    const csvContentType = 'text/csv';
+    const htmlContentType = 'text/html';
+
+    const contentType = response.headers.get('content-type');
+    console.log('Got the following content type from the submission of a multiple match file: "', contentType, '"');
+
+    // If it's a csv content type, then trigger the download 
+    if (contentType.includes(csvContentType)) {
+      console.log('The content type indicates this is a csv file, so triggering the download of the file');
+
+      // The response should be a file, so package it as a blob and create url
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+
+      // Create link and trigger download
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'file.csv';
+      a.click();
+
+      URL.revokeObjectURL(url);
+
+      // Remove loading button animation
+      removeLoadingClass(loadButton);
+    } else if (contentType.includes(htmlContentType)) {
+      console.log('The content type indicates this is an html file, so likely an error page, so navigating to the page');
+
+      // If it's an html content type, then navigate to the page (likely an error page)
+      const text = await response.text();
+      document.open();
+      document.write(text);
+      document.close();
+    } else {
+      console.log('Received a content type that is not csv or html, so not sure how to handle this response. Content type:', contentType);
     }
-    
-    // The response should be a file, so package it as a blob and create url
-    const blob = await response.blob();
-    const url = URL.createObjectURL(blob);
-
-    // Create link and trigger download
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'file.csv';
-    a.click();
-
-    URL.revokeObjectURL(url);
-
-    loadButton.classList.remove(isLoadingClassName);
 
   }
   ());
