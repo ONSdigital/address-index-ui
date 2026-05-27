6d2e962c408cacd7e438aadd45aaef78aa42c6b0
Felix Aldam-Gates <aldam.gatesfelix@gmail.com>
2026-05-27 18:15:34 +0100

    Adjusted filelocation

--- src/aims_ui/static/js/b_multiple_matches/multiple_address_results/init.mjs
+++ src/aims_ui/static/js/b_multiple_matches/multiple_address_results/init.mjs
@@ -1,7 +1,7 @@
 import { allPagesFirstInit } from '/static/js/e_all_pages/all_pages_first.mjs';
 import { allPagesLastInit } from '/static/js/e_all_pages/all_pages_last.mjs';
 import { setupResultsButtonAndProcessing } from './download_and_process/setup_download_buttons.mjs';
-import { addIncludeOldJobsFlagToUrlBasedOnLocalStorageSetting } from './download_and_process/add_include_old_jobs_flag.mjs';
+import { addIncludeOldJobsFlagToUrlBasedOnLocalStorageSetting } from './add_include_old_jobs_flag.mjs';
 
 export function init(page_name) {
   // All pages first
